35b0a740cd2f3124a1b22da310806ee6f6f742c2
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-01-12 02:05:25 -0600

    Correcciones en las Validaciones del Index
    Correcciones asies

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ let datosUsuario = [];
 
 
 const expresiones = {
-  nombreRegex: /^[a-zA-Z0-9À-ÿ\s]{1,40}$/,
+  nombreRegex: /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/,
   passwordRegex: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
-  emailRegex: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  emailRegex:  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
   telefonoRegex: /^(?!(0000000000|0000000001))\d{10}$/,
 }
 
@@ -23,44 +23,29 @@ const validarFormulario = (e) => {
         document.getElementById('inputNombre').classList.remove('inputIncorrecto')
         document.getElementById('inputNombre').classList.add('inputCorrecto')
         document.querySelector('#grupoNombre .alert-danger').classList.remove('alert-danger-activo')
-        return true
+        
       } else {
         document.getElementById('inputNombre').classList.remove('inputCorrecto')
         document.getElementById('inputNombre').classList.add('inputIncorrecto')
         document.querySelector('#grupoNombre .alert-danger').classList.add('alert-danger-activo')
-        return false
+        
       }
-      break;
-
-      if (expresiones.nombreRegex.test(e.target.value)) {
-
-        document.getElementById('inputnumeroTel').classList.remove('inputIncorrecto')
-        document.getElementById('inputnumeroTel').classList.add('inputCorrecto')
-        document.querySelector('#grupoTelefono .alert-danger').classList.remove('alert-danger-activo')
-        return true
-      } else {
-        document.getElementById('inputnumeroTel').classList.remove('inputCorrecto')
-        document.getElementById('inputnumeroTel').classList.add('inputIncorrecto')
-        document.querySelector('#grupoTelefono .alert-danger').classList.add('alert-danger-activo')
-        return false
-      }
-      break;
-
-
-    // case "inputnumeroTel":
-    //   if (expresiones.telefonoRegex.test(e.target.value)) {
-
-    //     document.getElementById('inputnumeroTel').classList.remove('inputIncorrecto')
-    //     document.getElementById('inputnumeroTel').classList.add('inputCorrecto')
-    //     document.querySelector('#grupoTelefono .alert-danger').classList.remove('alert-danger-activo')
-    //      return true
-    //     } else {
-    //     document.getElementById('inputnumeroTel').classList.remove('inputCorrecto')
-    //     document.getElementById('inputnumeroTel').classList.add('inputIncorrecto')
-    //     document.querySelector('#grupoTelefono .alert-danger').classList.add('alert-danger-activo')
-    //     return false
-    //     }
-      // break;
+      break;      
+    
+      case "inputTelefono":
+        if (expresiones.telefonoRegex.test(e.target.value)) {
+  
+          document.getElementById('inputTelefono').classList.remove('inputIncorrecto')
+          document.getElementById('inputTelefono').classList.add('inputCorrecto')
+          document.querySelector('#grupoTelefono .alert-danger').classList.remove('alert-danger-activo')
+          
+        } else {
+          document.getElementById('inputTelefono').classList.remove('inputCorrecto')
+          document.getElementById('inputTelefono').classList.add('inputIncorrecto')
+          document.querySelector('#grupoTelefono .alert-danger').classList.add('alert-danger-activo')
+          
+        }
+        break;      
 
     case "inputEmail":
       if (expresiones.emailRegex.test(e.target.value)) {
@@ -68,12 +53,12 @@ const validarFormulario = (e) => {
         document.getElementById('inputEmail').classList.remove('inputIncorrecto')
         document.getElementById('inputEmail').classList.add('inputCorrecto')
         document.querySelector('#grupoEmail .alert-danger').classList.remove('alert-danger-activo')
-        return true
+        
       } else {
         document.getElementById('inputEmail').classList.remove('inputCorrecto')
         document.getElementById('inputEmail').classList.add('inputIncorrecto')
         document.querySelector('#grupoEmail .alert-danger').classList.add('alert-danger-activo')
-        return false
+        
       }
       break;
     case "inputPassword":
@@ -82,12 +67,12 @@ const validarFormulario = (e) => {
         document.getElementById('inputPassword').classList.remove('inputIncorrecto')
         document.getElementById('inputPassword').classList.add('inputCorrecto')
         document.querySelector('#grupoPassword .alert-danger').classList.remove('alert-danger-activo')
-        return true
+        
       } else {
         document.getElementById('inputPassword').classList.remove('inputCorrecto')
         document.getElementById('inputPassword').classList.add('inputIncorrecto')
         document.querySelector('#grupoPassword .alert-danger').classList.add('alert-danger-activo')
-        return false
+        
       }
       break;
     case "inputPassword2":
@@ -97,12 +82,12 @@ const validarFormulario = (e) => {
         document.getElementById('inputPassword2').classList.remove('inputCorrecto')
         document.getElementById('inputPassword2').classList.add('inputIncorrecto')
         document.querySelector('#grupoPassword2 .alert-danger').classList.add('alert-danger-activo')
-        return false
+        
       } else {
         document.getElementById('inputPassword2').classList.remove('inputIncorrecto')
         document.getElementById('inputPassword2').classList.add('inputCorrecto')
         document.querySelector('#grupoPassword2 .alert-danger').classList.remove('alert-danger-activo')
-        return true
+        
       }
       break;
   }//switchid
@@ -111,6 +96,8 @@ const validarFormulario = (e) => {
 
 //Foreach.Inputs.Formulario - INICIA
 inputs.forEach((input) => {
+  input.addEventListener("mouseover", validarFormulario);
+  input.addEventListener("mouseout", validarFormulario);
   input.addEventListener("keyup", validarFormulario);
   input.addEventListener("blur", validarFormulario);
 });//ForEach.Inputs.Formulario - TERMINA
@@ -118,36 +105,32 @@ inputs.forEach((input) => {
 //Event.listener.btnRegistro - INICIA
 btnRegistro.addEventListener("click", function (event) {
   event.preventDefault();
-
   const nombre = inputNombre.value;
   const email = inputEmail.value;
   const password = inputPassword.value;
   const password2 = inputPassword2.value;
-  const telefono = inputnumeroTel.value;
-  if (!nombre) return alert("datos no validos");
-  if (!email) return alert("datos no validos");
-  if (!password) return alert("datos no validos");
-  if (!password2) return alert("datos no validos");
-  if (!telefono) return alert("datos no validos");
-
-  else {
-    alert("Datos correctos Mi amora <3");
-    // setDatos();
-    addItem(nombre, telefono, email, password, password2);
-    setLocal(datosUsuario);
-
-    window.location.href = "Publicaciones.html";
-  }
+  const telefono = inputTelefono.value;
+  if  (nombre && email && password && password2 && telefono){ 
+   window.location.href = "Publicaciones.html";
+   } else if (!nombre | !email | !password | !password2 | !telefono){
+    return alert ("Datos no válidos");
+  };
+ 
+//no se que sea esto att: abraham :v
   let inputFocused="";
 let elements = document.querySelectorAll("input[type='text'], input[type='password']");
 for (let i =0; i<elements.length;i++){
   elements[i].addEventListener("focus", function(){
     inputFocused = this;
-    inputFocused.value="";
+    inputFocused.valueOf="";
   });
-}
-
-})//Event.Listener.btnRegistro - TERMINA
+}//TERMINA
+//SetDatos - INICIA
+setDatos();
+  addItem(nombre, telefono, email, password, password2);
+  setLocal(datosUsuario);
+  //setDatos - TERMINA
+});//Event.Listener.btnRegistro - TERMINA
 
 //FUNCIONES LOCALSTORAGE
 
