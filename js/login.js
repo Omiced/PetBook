1606ecfd683be1b3b6afb184eabd426580830da5
Josue Cano <73917299+Omiced@users.noreply.github.com>
2023-01-16 17:35:17 +0000

    Agregadas alertas de validaciones del login Agregada función para validar el login

---
+++ js/login.js
@@ -0,0 +1,56 @@
+const btnRegistroEl = document.getElementById("btnRegistro");
+const divEl = document.querySelector(".alerts");
+const prueba = [
+  {
+    usuario: "Josue",
+    password: "josue1234",
+  },
+  {
+    usuario: "Bianca",
+    password: "bianca1234",
+  },
+];
+
+let usuariosArr = [];
+function renderAlert(message) {
+  const markup = `
+  <div class="alert alert-danger" role="error">
+    <h6>Datos incorrectos</h6>
+    <li>${message}</li>
+  </div>
+  `;
+
+  divEl.innerHTML = markup;
+}
+function validarDatos(arr, valueUsuario, valuePassword) {
+  return arr.some((registro) => {
+    if (
+      registro.usuario == valueUsuario &&
+      registro.password == valuePassword
+    ) {
+      return registro;
+    }
+    return;
+  });
+}
+
+function obtenerLocalStorage() {
+  const usuarios = localStorage.getItem("usuarios");
+  if (!usuarios) return;
+  usuariosArr = JSON.parse(usuarios);
+}
+btnRegistroEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  divEl.innerHTML = "";
+  const passwordValue = document.getElementById("inputPassword").value;
+  const usuarioValue = document.getElementById("input_nombre").value;
+  if (!passwordValue || !usuarioValue) {
+    renderAlert("Los campos no pueden quedar vacíos");
+    return;
+  }
+  if (!validarDatos(usuariosArr, usuarioValue, passwordValue)) {
+    renderAlert("El nombre de usuario o la contraseña son incorrectos");
+    return;
+  }
+  divEl.innerHTML = "";
+});
